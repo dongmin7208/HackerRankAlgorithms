f4004b4c7f7a1695f2cf4c359ce781944247c16e
dongmin7208 <sayg3o3d@gmail.com>
2022-06-28 00:50:49 +0900

    Compare-the-triplets

--- SolveMeFirst/SolveMeFirst.js
+++ SolveMeFirst/SolveMeFirst.js
@@ -20,7 +20,19 @@ function readLine() {
 
 function solveMeFirst(a, b) {
     // Hint: Type return a+b below
+
     return a + b;
+}
+function compareTriplets(a, b) {
+    let points = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            points[0]++;
+        } else if (a[i] != b[i]) {
+            points[1]++;
+        }
+    }
+    return points;
 }
 
 function main() {
@@ -29,4 +41,16 @@ function main() {
 
     var res = solveMeFirst(a, b);
     console.log(res);
+}
+
+function compareTriplets(a, b) {
+    let points = [0, 0];
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] > b[i]) {
+            points[0]++;
+        } else if (a[i] != b[i]) {
+            points[1]++;
+        }
+    }
+    return points;
 }
