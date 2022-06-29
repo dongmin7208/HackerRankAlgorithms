cfc71a4a490ee6a40ee471e05ff9e4e7c98c346d
dongmin7208 <sayg3o3d@gmail.com>
2022-06-30 00:22:33 +0900

    a-very-bigInt

--- a-very-big-sum/index.js
+++ a-very-big-sum/index.js
@@ -22,13 +22,6 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-/*
- * Complete the 'aVeryBigSum' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts LONG_INTEGER_ARRAY ar as parameter.
- */
-
 function aVeryBigSum(ar) {
     // Write your code here
     return ar.reduce((a, b) => BigInt(a) + BigInt(b));
